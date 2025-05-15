4dcc854e72521a4788d4b783b649108386cddebd
Porshen Lai <porshenlai@gmail.com>
2025-05-15 10:40:22 +0800

    20250515

---
+++ run_webserver.bat
@@ -0,0 +1,9 @@
+cd %~dp0\PWS
+
+if not exist "__pyenv__\python.xxx" (
+	powershell -c Set-ExecutionPolicy unrestricted -Scope CurrentUser
+	powershell -c ../bin/inst_py.ps1
+)
+
+__pyenv__\python.exe daemon.py config.json
+pause -1
